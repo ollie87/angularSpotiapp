3f1d9861a0bfcafe551804a5625408ce7e02dd98
Mario <mariogv87@gmail.com>
2019-08-12 11:02:11 +0200

    config router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ROUTES} from './app.routes';
 
-const routes: Routes = [];
+const routes: Routes = ROUTES;
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
